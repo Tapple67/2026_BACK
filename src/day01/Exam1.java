13311a97ce2c44e740c97d9bdc65873afeef0e93
JUN <guswns020607@naver.com>
2026-07-28 11:19:48 +0900

    저장

---
+++ src/day01/Exam1.java
@@ -0,0 +1,48 @@
+package day01;
+
+public class Exam {
+
+    //public : 공개용 , 다른 패키지에서도 사용가능한 클래스 뜻
+    //class : 클래스 선언 사용하는 키워드
+    // Exam : 클래스명은 아무거나 단 첫 글자는 대문자
+        //클래스는 자바에서의 가장 최소 컴파일/번역/통역사 단위
+
+
+
+        int a = 10;
+        
+
+        public static void main(String[] args) {
+
+
+            int b = 10;
+            System.out.println("안녕");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
